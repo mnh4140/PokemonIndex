fba2e713b2adc6de4ab759da7f4793367009c127
NH <mnh4140@gmail.com>
2025-05-15 10:27:28 +0900

    Initial Commit

---
+++ PokemonIndexTests/PokemonIndexTests.swift
@@ -0,0 +1,17 @@
+//
+//  PokemonIndexTests.swift
+//  PokemonIndexTests
+//
+//  Created by NH on 5/15/25.
+//
+
+import Testing
+@testable import PokemonIndex
+
+struct PokemonIndexTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
