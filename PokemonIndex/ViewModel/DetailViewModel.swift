a1b6ac664ada8fe1f09a5f368461d599637d1bd5
NH <mnh4140@gmail.com>
2025-05-15 21:53:09 +0900

    [Feat] #6 DetailViewModel 개발

--- PokemonIndex/ViewModel/DetailViewModel.swift
+++ PokemonIndex/ViewModel/DetailViewModel.swift
@@ -9,5 +9,25 @@ import Foundation
 import RxSwift
 
 class DetailViewModel {
+    let pokemonDetailSubject = PublishSubject<PokemonDetail>()
+    private let disposeBag = DisposeBag()
+    let id: Int
     
+    init(id: Int) {
+        self.id = id
+        fetchPokemonDetail(id: self.id)
+        print("ID:\(id)")
+    }
+    
+    func fetchPokemonDetail(id: Int) {
+        
+        guard let url = URL(string: "https://pokeapi.co/api/v2/pokemon/\(String(id))/") else { return }
+        
+        NetworkManager.shared.fetch(url: url)
+            .subscribe(onSuccess: { [weak self] (response: PokemonDetail) in
+                self?.pokemonDetailSubject.onNext(response)
+            }, onFailure: { [weak self] error in
+                self?.pokemonDetailSubject.onError(error)
+            }).disposed(by: disposeBag)
+    }
 }
